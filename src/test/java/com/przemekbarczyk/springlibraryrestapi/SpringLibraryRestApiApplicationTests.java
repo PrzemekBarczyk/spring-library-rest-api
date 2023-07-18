ab723e2f40ea9880816af838775c6d7ac7d81a76
PrzemekBarczyk <przemek.barczyk@gmail.com>
2023-07-18 21:26:09 +0200

    Initial commit

---
+++ src/test/java/com/przemekbarczyk/springlibraryrestapi/SpringLibraryRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.przemekbarczyk.springlibraryrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
