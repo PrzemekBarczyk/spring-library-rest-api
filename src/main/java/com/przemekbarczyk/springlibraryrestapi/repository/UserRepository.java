80b922d5c0f2633cd5bf379a7be4bc3ac4558c4e
PrzemekBarczyk <przemek.barczyk@gmail.com>
2023-07-19 22:04:00 +0200

    Add UserRepository

---
+++ src/main/java/com/przemekbarczyk/springlibraryrestapi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.przemekbarczyk.springlibraryrestapi.repository;
+
+import com.przemekbarczyk.springlibraryrestapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+}
