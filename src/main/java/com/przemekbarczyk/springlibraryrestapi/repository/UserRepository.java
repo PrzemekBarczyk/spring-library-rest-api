a41ca77725b32f6f3ee55422a5e0cd793d39ec28
PrzemekBarczyk <przemek.barczyk@gmail.com>
2023-07-20 12:12:39 +0200

    Edit UserRepository
    - added two new methods

--- src/main/java/com/przemekbarczyk/springlibraryrestapi/repository/UserRepository.java
+++ src/main/java/com/przemekbarczyk/springlibraryrestapi/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package com.przemekbarczyk.springlibraryrestapi.repository;
 
 import com.przemekbarczyk.springlibraryrestapi.model.User;
+import com.przemekbarczyk.springlibraryrestapi.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByIdAndRole(Long id, UserRole role);
 }
