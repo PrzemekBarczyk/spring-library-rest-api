b2cc37caa61f7bfa9c1034e0c000067da7b5620e
PrzemekBarczyk <przemek.barczyk@gmail.com>
2023-07-21 17:16:53 +0200

    Edit SpecificationUtility
    - add specification for book

--- src/main/java/com/przemekbarczyk/springlibraryrestapi/utility/SpecificationUtility.java
+++ src/main/java/com/przemekbarczyk/springlibraryrestapi/utility/SpecificationUtility.java
@@ -10,6 +10,45 @@ import java.util.List;
 
 public class SpecificationUtility {
 
+    public Specification<Book> getBookSpecification(Book book) {
+
+        return (root, query, criteriaBuilder) -> {
+
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (book != null) {
+                if (book.getTitle() != null) {
+                    predicates.add(criteriaBuilder.equal(
+                            criteriaBuilder.lower(root.get("title")),
+                            book.getTitle().toLowerCase()));
+                }
+                if (book.getAuthor() != null) {
+                    predicates.add(criteriaBuilder.equal(
+                            criteriaBuilder.lower(root.get("author")),
+                            book.getAuthor().toLowerCase()));
+                }
+                if (book.getPublisher() != null) {
+                    predicates.add(criteriaBuilder.equal(
+                            criteriaBuilder.lower(root.get("publisher")),
+                            book.getPublisher().toLowerCase()));
+                }
+                if (book.getPublicationDate() != null) {
+                    predicates.add(criteriaBuilder.equal(root.get("publicationDate"),book.getPublicationDate()));
+                }
+                if (book.getStatus() != null) {
+                    predicates.add(criteriaBuilder.equal(
+                            criteriaBuilder.lower(root.get("status")),
+                            book.getStatus().name().toLowerCase()));
+                }
+                if (book.getReader() != null) {
+                    predicates.add(criteriaBuilder.equal(root.get("reader").get("id"), (book.getReader())));
+                }
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+
     public Specification<User> getUserSpecification(User user) {
 
         return (root, query, criteriaBuilder) -> {
