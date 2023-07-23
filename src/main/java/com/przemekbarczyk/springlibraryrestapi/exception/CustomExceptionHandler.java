aab08e15323d940b3b63a8c7dbc3bcc5e3510afd
PrzemekBarczyk <przemek.barczyk@gmail.com>
2023-07-23 19:35:44 +0200

    Add CustomExceptionHandler

---
+++ src/main/java/com/przemekbarczyk/springlibraryrestapi/exception/CustomExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.przemekbarczyk.springlibraryrestapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Objects;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        return new ResponseEntity<>(ex.getReason(), ex.getStatusCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return new ResponseEntity<>(
+                Objects.requireNonNull(ex.getBindingResult().getFieldError()).getDefaultMessage(),
+                HttpStatus.BAD_REQUEST);
+    }
+}
