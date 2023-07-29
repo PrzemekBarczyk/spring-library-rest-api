ac06ed7be196da3d49bea2323dcff60e7ea2f681
PrzemekBarczyk <przemek.barczyk@gmail.com>
2023-07-29 23:37:09 +0200

    Edit AuthenticationService
    - added new catch clause for invalid email

--- src/main/java/com/przemekbarczyk/springlibraryrestapi/service/AuthenticationService.java
+++ src/main/java/com/przemekbarczyk/springlibraryrestapi/service/AuthenticationService.java
@@ -5,7 +5,9 @@ import com.przemekbarczyk.springlibraryrestapi.security.UserPrincipal;
 import com.przemekbarczyk.springlibraryrestapi.utility.JWTTokenUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -33,7 +35,10 @@ public class AuthenticationService {
 
             return jwtTokenUtility.generateAccessToken(principal.getId(), principal.getEmail(), role);
         }
-        catch (IllegalArgumentException ex) {
+        catch (InternalAuthenticationServiceException ex) { // invalid email
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials");
+        }
+        catch (IllegalArgumentException ex) { // invalid password
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials");
         }
     }
