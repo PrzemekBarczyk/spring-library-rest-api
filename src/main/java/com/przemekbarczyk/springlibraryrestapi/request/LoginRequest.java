9644a3ca2233d5d06c5a6a92de5e2be51e37f6de
PrzemekBarczyk <przemek.barczyk@gmail.com>
2023-07-29 23:35:33 +0200

    Edit LoginRequest
    - added NotNull annotation for password

--- src/main/java/com/przemekbarczyk/springlibraryrestapi/request/LoginRequest.java
+++ src/main/java/com/przemekbarczyk/springlibraryrestapi/request/LoginRequest.java
@@ -1,6 +1,7 @@
 package com.przemekbarczyk.springlibraryrestapi.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -10,6 +11,7 @@ public class LoginRequest {
     @NotBlank(message = "Email is empty or blank")
     private String email;
 
+    @NotNull(message = "Password has less than 6 characters")
     @Size(min = 6, message = "Password has less than 6 characters")
     private String password;
 }
